3ec4310feca0dd898efd564ec73f6a9b26355d95
Pete Richardson <pete@peterichardson.com>
2016-03-27 12:45:25 -0700

    Moved extensions of NSDate and EKReminder to their own file: extensions.swift

--- src/main.swift
+++ src/main.swift
@@ -1,33 +1,5 @@
 import EventKit
 
-public func ==(lhs: NSDate, rhs: NSDate) -> Bool {
-    return lhs === rhs || lhs.compare(rhs) == .OrderedSame
-}
-
-public func <(lhs: NSDate, rhs: NSDate) -> Bool {
-    return lhs.compare(rhs) == .OrderedAscending
-}
-
-extension NSDate: Comparable {
-    public func dateWithoutTime() -> NSDate {
-        let comps = NSCalendar.currentCalendar().components([.Year, .Month, .Day], fromDate: self)
-        return NSCalendar.currentCalendar().dateFromComponents(comps)!
-    }
-}
-
-extension EKReminder {
-    public override var description: String {
-        let completed : String = self.completed ? "✔" : " "
-        let hash = NSString(format:"%02X", self.hash & 0xFF)
-        return "\(completed)  \(hash) \(self.title)"
-    }
-    
-    public var completedToday: Bool {
-        return self.completed && self.completionDate!.dateWithoutTime() == NSDate().dateWithoutTime()
-    }
-}
-
-
 do {
     var eventStore : EKEventStore = EKEventStore()
     eventStore.requestAccessToEntityType(EKEntityType.Reminder, completion: {
