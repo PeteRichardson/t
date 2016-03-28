5a51c4733176ab56ecf360e21bcafac0be3eb158
Pete Richardson <pete@peterichardson.com>
2016-03-28 09:09:23 -0700

    Introduce dependency on ncurses

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "t"
+    name: "t",
+    dependencies: [
+        .Package(url: "https://github.com/Baltoli/Cncurses.git", majorVersion: 1)
+    ]
 )
