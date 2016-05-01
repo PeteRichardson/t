b3aac1f7872b9cf83a13ba2581bfa813add232cb
Pete Richardson <pete@peterichardson.com>
2016-04-30 21:34:58 -0700

    Change to my own copy of the NCurses glue.  The original got deleted

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "t",
     dependencies: [
-        .Package(url: "https://github.com/Baltoli/Cncurses.git", majorVersion: 1)
+        .Package(url: "https://github.com/PeteRichardson/Cncurses.git", majorVersion: 1)
     ]
 )
