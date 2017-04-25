ce615fac889304db917dee809d2eefa859bcde08
Pete Richardson <peterichardson@apple.com>
2017-04-25 14:50:50 -0700

    Simplified argparsing code in main

--- src/main.swift
+++ src/main.swift
@@ -1,5 +1,18 @@
 import EventKit
 
+let priority_map = [
+	"uih":  1,
+	"ui":   2,
+	"uil":  3,
+	"nuih": 4,
+	"nui":  5,
+	"nuil": 6,
+	"unih": 7,
+	"uni":  8,
+	"unil": 9,
+	"nuni": 0
+]
+
 do {
 	var view = EisenhowerConsoleView()
 
@@ -8,51 +21,26 @@ do {
     var args = CommandLine.arguments
     args.remove(at:0)
     if args.count > 0 {
-        switch (args[0]).lowercased() {
+    	let words = Array(args.suffix(args.count-1))
+    	let command = args[0].lowercased()
+        switch command {
 	        case "c":
-	            args.remove(at: 0)
-	            try remCache.complete_reminder(args: args)
+	            try remCache.complete_reminder(args: words)
 	        case "d":
-	            args.remove(at: 0)
-	            try remCache.delete_reminder(args: args)
-	        case "uih":
-	            args.remove(at: 0)
-	            remCache.add_reminder(args: args, priority: 1)
-	        case "ui":
-	            args.remove(at: 0)
-	            remCache.add_reminder(args: args, priority: 2)
-	        case "uil":
-	            args.remove(at: 0)
-	            remCache.add_reminder(args: args, priority: 3)
-	        case "nuih":
-	            args.remove(at: 0)
-	            remCache.add_reminder(args: args, priority: 4)
-	        case "nui":
-	            args.remove(at: 0)
-	            remCache.add_reminder(args: args, priority: 5)
-	        case "nuil":
-	            args.remove(at: 0)
-	            remCache.add_reminder(args: args, priority: 6)
+	            try remCache.delete_reminder(args: words)
 	        case "m":
-	            args.remove(at: 0)
-	            remCache.move_reminder(args: args)
-	        case "unih":
-	            args.remove(at: 0)
-	            remCache.add_reminder(args: args, priority: 7)
-	        case "uni":
-	            args.remove(at: 0)
-	            remCache.add_reminder(args: args, priority: 8)
-	        case "unil":
-	            args.remove(at: 0)
-	            remCache.add_reminder(args: args, priority: 9)
-	        case "nuni":
-	            args.remove(at: 0)
-	            remCache.add_reminder(args: args, priority: 0)
+	            remCache.move_reminder(args: words)
+	        case  "uih",  "ui",  "uil",
+	        	 "nuih", "nui", "nuil",
+	        	 "unih", "uni", "unil",
+	        	 "nuni":
+	            remCache.add_reminder(args: words, priority: priority_map[command]!)
 	        default:
 	            remCache.add_reminder(args: args, priority: 0)
         }
     }
     remCache.loadItems()
+    
     view.display(uiItems: remCache.uiItems, nuiItems: remCache.nuiItems, uniItems:remCache.uniItems, nuniItems: remCache.nuniItems, leftMaxWidth: remCache.leftMaxWidth, rightMaxWidth: remCache.rightMaxWidth)
 
 } catch {
