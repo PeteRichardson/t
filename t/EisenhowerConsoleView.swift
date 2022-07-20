07cd818f53cca0266ce667b882b6e6b4f1c8cc66
Pete Richardson <pete@peterichardson.com>
2022-07-19 22:50:06 -0700

    Minor: Refactoring, cleanup, comments

--- t/EisenhowerConsoleView.swift
+++ t/EisenhowerConsoleView.swift
@@ -3,9 +3,10 @@ import Darwin
 
 
 public class EisenhowerConsoleView {
-	var cols:Int = 80
-	var leftcols:Int
-	var rightcols:Int
+	var cols: Int           // total columns we have to work with.
+	var leftcols:  Int      // final width of the left column (considering lengths of all left items)
+	var rightcols: Int      // final width of the right column
+    
     // Note: these ...MaxWidth properties probably don't benefit from the lazy
     // keyword.  They are each executed exactly once for each execution,
     // and they aren't particularly slow.  User wouldn't notice.
@@ -67,55 +68,78 @@ public class EisenhowerConsoleView {
         var w = winsize()
         if ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == 0 {
             self.cols = Int(w.ws_col)
+        } else {
+            self.cols = 80
         }
         self.leftcols = Int(Double(self.cols) * 0.5)
 		self.rightcols = Int(self.cols) - self.leftcols
         self.remCache = reminders
 	}
 
+    /**
+     Build the text for a single reminder, including padding
+     
+     Made up of 4 fields:
+        1. check box if completed
+        2. a 3 digit hex hash
+        3. a 0-9 digit for priority (see priority_map above)
+        4. the reminder text (aka title)
+
+     e.g. " ✔  A43 8 Eat Breakfast           "
+     */
     func format(rem: EKReminder, width: Int) -> String {
         let checkColor = 120
         let hashColor  = 210
-        let titleColorNotCompleted = 255
-        let titleColorCompleted = 238
 
-        let completed : String = rem.isCompleted ? "\u{1B}[38;5;\(checkColor)m✔\u{1B}[m" : " "
-        let hash = "\u{1B}[38;5;\(hashColor)m\(rem.key)\u{1B}[m"
-        let titleColor:Int = rem.isCompleted ? titleColorCompleted : titleColorNotCompleted
-        var titleText:String = String(rem.priority) + " " + rem.title + String(repeating: " ", count: width)
-        let index = titleText.index(titleText.startIndex, offsetBy: width-4)
-        titleText = String(titleText[..<index])
+        let completed: String = rem.isCompleted ? "\u{1B}[38;5;\(checkColor)m✔\u{1B}[m" : " "   // colored check box
+        
+        let hash = "\u{1B}[38;5;\(hashColor)m\(rem.key)\u{1B}[m"                                // colored hash hex
+        
+        let priority = String(rem.priority)
+        
+        let titleColor: Int = rem.isCompleted ? 238 : 255                                       // color of reminder title text
+        let titleText = (rem.title + String(repeating: " ", count: width)).prefix(width-6)
         let title = "\u{1B}[38;5;\(titleColor)m\(titleText)\u{1B}[m"
-        return "\(completed)  \(hash) \(title)"
+        
+        return "\(completed)  \(hash) \(priority) \(title)"
+    }
+    
+    /**
+     Order reminders grouped by completion (uncompleted first, then completed)
+     and ordered by priority (highest to lowest) within each group
+     */
+    func sortByCompletionAndPriority(r1: EKReminder, r2: EKReminder) -> Bool {
+        if r1.isCompleted == r2.isCompleted {   // if both are complete or incomplete...
+            return r1.priority < r2.priority    // then order by priority
+        } else {
+            return r2.isCompleted               // else order the incomplete one first
+        }
     }
 
-
-	func list_reminders_side_by_side(list1: ReminderDict, list2: ReminderDict, width1: Int, width2: Int) {
-        let rowCount = list1.count > list2.count ? list1.count : list2.count
-        let sortedList1 = list1.values.sorted() { (r1, r2) in
-            if r1.isCompleted == r2.isCompleted {
-                return r1.priority < r2.priority
-            } else {
-                return r2.isCompleted
-            }
-        }
-        let sortedList2 = list2.values.sorted() { (r1, r2) in
-            if r1.isCompleted == r2.isCompleted {
-                return r1.priority < r2.priority
-            } else {
-                return r2.isCompleted
-            }
-        }
+	/**
+     Print one half of the table (top or bottom)
+     This just takes care of the reminder lines themselves.   The horizontal dividers
+     are printed directly in display()
+     
+     TODO:  clean up all the "magic numbers" in this func.
+     */
+    func list_reminders_side_by_side(list1: ReminderDict, list2: ReminderDict, width1: Int, width2: Int) {
+        let rowCount = max(list1.count, list2.count)
+        let sortedList1 = list1.values.sorted(by: sortByCompletionAndPriority)
+        let sortedList2 = list2.values.sorted(by: sortByCompletionAndPriority)
         
         for row in 0..<rowCount {
-            var left: String = String(repeating: " ", count: width1 - 4)
-            if row < sortedList1.count {
-                left = self.format(rem: sortedList1[row], width: width1-7)
+            // Figure out what to print on the left side (either spaces or a reminder)
+            var left: String  = String(repeating: " ", count: width1 - 4)     // Assume no item to print on the left (i.e. just print spaces)
+            if row < sortedList1.count {                                      // but, if there _is_ an item,
+                left =  self.format(rem: sortedList1[row], width: width1 - 7) // print it instead.
             }
-            var right: String = String(repeating: " ", count: width2-3)
+            // Figure out what to print on the right side
+            var right: String = String(repeating: " ", count: width2 - 3)     // Same for the text on the right
             if row < sortedList2.count {
-                right = self.format(rem: sortedList2[row], width: width2-6)
+                right = self.format(rem: sortedList2[row], width: width2 - 6)
             }
+            // print left and right separated by vertical bars
             print("\u{2502} \(left) \u{2502} \(right) \u{2502}")
         }
     }
@@ -124,18 +148,15 @@ public class EisenhowerConsoleView {
 	func display() {
         // Use box drawing characters to make a prettier border around the lists.
         // see https://en.wikipedia.org/wiki/Box-drawing_character
-        if self.leftMaxWidth < self.leftcols {
-            self.leftcols = self.leftMaxWidth
-        }
-        if self.rightMaxWidth < self.rightcols {
-            self.rightcols = self.rightMaxWidth
-        }
+        leftcols  = max(leftMaxWidth,  leftcols)
+        rightcols = max(rightMaxWidth, rightcols)
+
         if self.leftMaxWidth + self.rightMaxWidth <= self.cols {
             self.leftcols = self.leftMaxWidth
             self.rightcols = self.rightMaxWidth
        }
         
-        let leftcolborder = String(repeating: "\u{2500}", count: self.leftcols - 2)
+        let leftcolborder  = String(repeating: "\u{2500}", count: self.leftcols  - 2)
         let rightcolborder = String(repeating: "\u{2500}", count: self.rightcols - 1)
         
         print("\u{1B}[m")
